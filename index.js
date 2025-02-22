a247a1263dbd6dffe74eda4934c0f934319c950c
stellalphatic <beingshahzaib26@gmail.com>
2025-02-22 21:05:04 +0500

    added read route

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 //creating route for create
 app.post("/create",(req,res)=>{
     // title and description are name of input fields
-    fs.writeFile(`./files/${req.body.title.split(" ").join("-")}.txt`,req.body.description,(err)=>{
+    fs.writeFile(`./files/${req.body.title.split(" ").join("-")}`,req.body.description,(err)=>{
         if(err){
             console.log(err);
         }else{
@@ -31,7 +31,20 @@ app.post("/create",(req,res)=>{
     // console.log(req.body);
     // res.send("data received");
 })
-
+//creating route for read
+app.get("/files/:filename",(req,res)=>{
+    const file=req.params.filename;
+    const content=fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,data)=>{
+        res.render("read",{content:data,file:file});
+    })
+})
+//creating route for edit
+app.get("/edit/:filename",(req,res)=>{
+    const file=req.params.filename;
+    const content=fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,data)=>{
+        res.render("edit",{content:data,file:file});
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
