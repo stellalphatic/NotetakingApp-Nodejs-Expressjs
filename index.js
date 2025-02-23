69eb3a5ac67450155ec96e1c425dc79ecedc5407
stellalphatic <beingshahzaib26@gmail.com>
2025-02-23 13:50:12 +0500

    added route for done tasks

--- index.js
+++ index.js
@@ -10,16 +10,17 @@ app.set('views',path.join(__dirname,'views'));
 
 
 app.get("/", (req, res) => {
-    fs.readdir('./files',(err,files)=>{
-        //   console.log(files);
-        res.render("index",{files:files});
+    fs.readdir('./files/tasks',(err,files)=>{
+        fs.readdir('./files/done',(err,done)=>{
+            res.render("index",{files:files,done:done});
+        })
     })
 
 });
 //creating route for create
 app.post("/create",(req,res)=>{
     // title and description are name of input fields
-    fs.writeFile(`./files/${req.body.title.split(" ").join("-")}`,req.body.description,(err)=>{
+    fs.writeFile(`./files/tasks/${req.body.title.split(" ").join("-")}`,req.body.description,(err)=>{
         if(err){
             console.log(err);
         }else{
@@ -34,20 +35,20 @@ app.post("/create",(req,res)=>{
 //creating route for read
 app.get("/files/:filename",(req,res)=>{
     const file=req.params.filename;
-    const content=fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,data)=>{
+    const content=fs.readFile(`./files/tasks/${req.params.filename}`,"utf-8",(err,data)=>{
         res.render("read",{content:data,file:file});
     })
 })
 //creating route for edit
 app.get("/edit/:filename",(req,res)=>{
     const file=req.params.filename;
-    const content=fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,data)=>{
+    const content=fs.readFile(`./files/tasks/${req.params.filename}`,"utf-8",(err,data)=>{
         res.render("edit",{content:data,file:file});
     })
 })
 app.post("/edit/:filename",(req,res)=>{
-    const oldFile = `./files/${req.params.filename}`;
-    const newFile = `./files/${req.body.title.split(" ").join("-")}`;
+    const oldFile = `./files/tasks/${req.params.filename}`;
+    const newFile = `./files/tasks/${req.body.title.split(" ").join("-")}`;
     
     if (fs.existsSync(oldFile)) {
         fs.renameSync(oldFile, newFile);  // Rename instead of creating a new file
@@ -65,11 +66,36 @@ app.post("/edit/:filename",(req,res)=>{
 })
 //deleting route
 app.get("/delete/:filename",(req,res)=>{
-    fs.unlink(`./files/${req.params.filename}`,(err)=>{
+    fs.unlink(`./files/tasks/${req.params.filename}`,(err)=>{
         res.redirect("/");
     })
 })
-
+//done route
+app.get("/done/:filename",(req,res)=>{
+    fs.readFile(`files/tasks/${req.params.filename}`,"utf-8",(err,data)=>{
+        fs.writeFile(`files/done/${req.params.filename}`,data,(err)=>{
+            if(err){
+                console.log(err);
+            }else{
+                console.log("done");
+            }
+        })
+        fs.unlink(`files/tasks/${req.params.filename}`,(err)=>{
+            if(err){
+                console.log(err);
+            }else{
+                console.log("deleted");
+            }
+        })
+        res.redirect("/");
+    })
+})
+//deleting done route
+app.get("/done/delete/:filename",(req,res)=>{
+    fs.unlink(`files/done/${req.params.filename}`,(err)=>{
+        res.redirect("/");
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
